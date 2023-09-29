29c34279b5dc8651fb1a4e13ae342adf563bb62a
theguywhodoesntcare <stormshadow4101@gmail.com>
2023-09-29 22:46:08 +0300

    Initial

---
+++ io_warcraft_mrf/__init__.py
@@ -0,0 +1,188 @@
+bl_info = {
+    "name": "Warcraft MORF (.mrf) Importer",
+    "author": "poisoNDealer",
+    "version": (0, 2),
+    "blender": (3, 6, 2),
+    "location": "File > Import > Warcraft MORF (.mrf)",
+    "description": "Imports Warcraft .mrf files and reproduces their animations using Shape Keys",
+    "warning": "",
+    "wiki_url": "",
+    "category": "Import-Export",
+}
+
+import bpy
+from bpy_extras.io_utils import ImportHelper
+from bpy_extras.io_utils import ExportHelper
+from bpy.types import Operator
+
+class MessageBox:
+    @staticmethod
+    def show(message="", title="Message Box", icon='INFO'):
+        def draw(self, context):
+            self.layout.label(text=message)
+
+        bpy.context.window_manager.popup_menu(draw, title=title, icon=icon)
+
+from . import import_mrf
+from . import export_mrf
+
+
+class ImportMRFOperator(Operator, ImportHelper):
+    bl_idname = "import.mrf"
+    bl_label = "Import MRF"
+    filename_ext = ".mrf"
+
+    filter_glob: bpy.props.StringProperty(
+        default="*.mrf",
+        options={'HIDDEN'},
+    )
+
+    divisor: bpy.props.FloatProperty(
+        name="Divisor",
+        description="The scale factor (division) of the imported model",
+        default=50,
+        min = 1.0
+    )
+
+    shade_smooth: bpy.props.BoolProperty(
+        name="Shade Smooth",
+        description="Apply smooth shading to the imported model",
+        default=False,
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        box = layout.box()
+        box.label(text="Warcraft sizes will be too large for Blender!", icon='ERROR')
+        box.label(text="Use a divisor to reduce model by N times")
+        layout.prop(self, 'divisor')
+        layout.prop(self, 'shade_smooth')
+
+    def execute(self, context):
+        #print(f"Selected file: {self.filepath}")
+        import_mrf.load_morf(self.filepath, self.divisor, self.shade_smooth)
+        return {'FINISHED'}
+
+class ExportMRFOperator(Operator, ExportHelper):
+    bl_idname = "export.mrf"
+    bl_label = "Export MRF"
+    filename_ext = ".mrf"
+    default_texture = r'Textures/white'
+
+    filter_glob: bpy.props.StringProperty(
+        default="*.mrf",
+        options={'HIDDEN'},
+    )
+
+    scale_factor: bpy.props.FloatProperty(
+        name="Scale Factor",
+        description="The scale factor of the exported model",
+        default=50,
+        min = 0.01
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        box = layout.box()
+        box.label(text="Blender sizes will be too small for Warcraft!", icon='ERROR')
+        box.label(text="Use a Scale Factor to scale model by N times")
+        layout.prop(self, 'scale_factor')
+
+    def execute(self, context):
+        #print(f"Selected file: {self.filepath}")
+
+        # ===Get Texture Path===
+        mat = bpy.context.object.active_material
+        texture_path = ExportMRFOperator.default_texture
+
+        def texture_warning():
+            MessageBox.show(f"The path {ExportMRFOperator.default_texture} has been set", 'Texture path not found. Set the MRF texture path', 'ERROR')
+        
+        if mat is not None:
+            if len(mat.mrf_texture_props.texture_path) > 0:
+                texture_path = mat.mrf_texture_props.texture_path
+                texture_path = r"{}".format(texture_path)
+            else:
+                texture_warning()
+        else:
+            texture_warning()
+
+        # ===Get KF Range===
+        kf_start = 0
+        kf_end = 24
+        markers = bpy.context.scene.timeline_markers
+
+        mrf_markers = [marker for marker in markers if marker.name.lower() == "mrf"]
+
+        if len(mrf_markers) >= 2:
+            mrf_markers.sort(key=lambda marker: marker.frame)
+            kf_start = mrf_markers[0].frame
+            kf_end = mrf_markers[1].frame
+        else:
+            MessageBox.show(f"Default Keyframe Range {kf_start} — {kf_end} has been set", 'MRF Markers not found!', 'ERROR')
+
+        print(kf_start)
+        print(kf_end)        
+        export_mrf.save_morf(self.filepath, self.scale_factor, texture_path, (kf_start, kf_end))
+        return {'FINISHED'}
+
+
+def update_texture_path(self, context):
+    print("Texture path updated to", self.texture_path)
+
+class MRFTextureProperties(bpy.types.PropertyGroup):
+    texture_path: bpy.props.StringProperty(
+        name="Path",
+        description="Path to the texture file",
+        default="",
+        maxlen=1024,
+        subtype='NONE',
+        update=update_texture_path
+    )
+
+class MATERIAL_PT_mrf_texture(bpy.types.Panel):
+    bl_label = "MRF Texture"
+    bl_context = "material"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    
+    @classmethod
+    def poll(cls, context):
+        return context.material is not None
+
+    def draw(self, context):
+        layout = self.layout
+        mat = context.material
+        box = layout.box()
+        box.prop(mat.mrf_texture_props, "texture_path")
+
+def draw_func(self, context):
+    layout = self.layout
+    mat = context.material
+    if mat is not None:
+        box = layout.box()
+        box.label(text="MRF Texture")
+        box.prop(mat.mrf_texture_props, "texture_path")
+
+def menu_func_import(self, context):
+    self.layout.operator(ImportMRFOperator.bl_idname, text="Warcraft MORF (.mrf)")
+
+def menu_func_export(self, context):
+    self.layout.operator(ExportMRFOperator.bl_idname, text="Warcraft MORF (.mrf)")
+
+def register():
+    bpy.utils.register_class(MRFTextureProperties)
+    bpy.utils.register_class(MATERIAL_PT_mrf_texture)
+    bpy.utils.register_class(ImportMRFOperator)
+    bpy.utils.register_class(ExportMRFOperator)
+    bpy.types.Material.mrf_texture_props = bpy.props.PointerProperty(type=MRFTextureProperties)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+def unregister():
+    bpy.utils.unregister_class(MRFTextureProperties)
+    bpy.utils.unregister_class(MATERIAL_PT_mrf_texture)
+    bpy.utils.unregister_class(ImportMRFOperator)
+    bpy.utils.unregister_class(ExportMRFOperator)
+    del bpy.types.Material.mrf_texture_props
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
---
+++ io_warcraft_mrf/export_mrf.py
@@ -0,0 +1,239 @@
+import os
+import struct
+import bpy
+import bmesh
+from mathutils import geometry
+from . import MessageBox
+#os.system('cls')
+
+def save_morf(filepath, scale_factor, texture_path, kf_range):
+    def set_dword(data, value):
+        data += struct.pack('<I', value)
+        return data
+
+    def set_float(data, value):
+        data += struct.pack('<f', value)
+        return data
+
+    def set_str(data, value):
+        b = value.encode('utf-8')
+        data += struct.pack('%ds' % len(b), b)
+        return data
+
+    def set_vector(data, vector):
+        data += struct.pack('<fff', *vector)
+        return data
+
+    def set_triangle(data, triangle):
+        data += struct.pack('<HHH', *triangle)
+        return data
+
+    def set_uv(data, uv):
+        data += struct.pack('<ff', *uv)
+        return data
+
+    def pad_chunk(data):
+        #Fill the chunk with zeros so that its length is a multiple of 16
+        padding = 16 - (len(data) % 16)
+        
+        if padding == 16:
+            return data
+
+        data.extend([0]*padding)
+        return data
+
+    def pad_chunk_size(data, chunk_size):
+        #Fill the chunk with zeros so that its size is equal to the specified size
+        padding = chunk_size - (len(data) % chunk_size)
+        
+        if padding == chunk_size:
+            return data
+
+        data.extend([0]*padding)
+        return data
+
+    def write_data_chunk(data, nkeyframes, nverts, nfaces, duration):
+        set_str(data, 'Morf') #mrf id
+        set_dword(data, nkeyframes) #Number of keyframes
+        set_dword(data, nverts) #Number of vertices
+        set_dword(data, nfaces * 3) #Number of vertices of triangles
+        set_float(data, duration) #frame duration (1/fps)
+        set_vector(data, (0, 0, 0)) # (?) Maybe Pivot
+        set_float(data, 30) # (?) Maybe Bounds Radius
+
+        data = pad_chunk_size(data, 64)
+
+        return data
+
+    def write_texture(path):
+        data = bytearray()
+        data = set_str(data, path)
+        data = pad_chunk(data)
+        return data
+    
+    def get_keyframes(obj, frame_start, frame_end):
+        #Get vertex position and normals from every frame
+        number = frame_end - frame_start + 1
+
+        sce = bpy.context.scene
+        animdata = bytearray()
+
+        for f in range(frame_start, frame_end + 1):  #set frame range
+            sce.frame_set(f)
+            
+            depsgraph = bpy.context.evaluated_depsgraph_get()
+            
+            mesh = obj.evaluated_get(depsgraph).to_mesh()
+            
+            framedata = bytearray()
+            
+            for v in mesh.vertices:
+                global_coord = obj.matrix_world @ v.co
+                global_coord = global_coord * scale_factor
+                framedata = set_vector(framedata, global_coord)
+                
+                normal = v.normal
+                framedata = set_vector(framedata, normal)
+            
+            framedata = pad_chunk(framedata)
+            animdata.extend(framedata)
+            
+            obj.evaluated_get(depsgraph).to_mesh_clear()
+            
+        fps = bpy.context.scene.render.fps #get fps
+        dur = 1/fps
+
+        length = len(animdata)//number
+        print('frame length: ', length)
+        return animdata, number, dur, length
+
+    def get_triangle_representation(obj):
+        #Get faces in a triangular representation.
+        #Currently the mesh is forced to be triangulated before export, so this is unnecessary
+        #But in the future, triangulation must be excluded
+
+        bpy.ops.object.mode_set(mode='EDIT')
+        bm = bmesh.from_edit_mesh(obj.data)
+
+        triangle_count = 0
+        data = bytearray()
+        for i, face in enumerate(bm.faces):
+            triangle = ()
+            #If a face has more than 3 vertices, divide it into triangles
+            if len(face.verts) > 3:
+                coords = [v.co for v in face.verts]
+                triangles = geometry.tessellate_polygon([coords])
+
+                for tri in triangles:
+                    #print(f"face {i} = {tuple(face.verts[v].index for v in tri)}")
+                    triangle = tuple(face.verts[v].index for v in tri)
+                    triangle_count += 1
+                    data = set_triangle(data, triangle)
+            else:
+                #print(f"face {i} = {tuple(v.index for v in face.verts)}")
+                triangle = tuple(v.index for v in face.verts)
+                triangle_count += 1
+                data = set_triangle(data, triangle)
+
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+        #print(f"Total number of triangles: {triangle_count}")
+
+        data = pad_chunk(data)
+        return data, triangle_count
+
+
+    def get_uv_coords(obj):
+        #why tf get uv is so hard?
+        data = bytearray()
+        if bpy.context.mode != 'EDIT_MESH':
+            bpy.ops.object.mode_set(mode='EDIT')
+
+        me = obj.data
+        bm = bmesh.from_edit_mesh(me)
+
+        uv_layer = bm.loops.layers.uv.verify()
+
+        unique_uvs = {}
+
+        for v in bm.verts:
+            for l in v.link_loops:
+                luv = l[uv_layer]
+
+                if v.index in unique_uvs:
+                    continue
+
+                unique_uvs[v.index] = luv.uv
+
+        for vert_index, uv in unique_uvs.items():
+            data = set_uv(data, uv)
+            #print(f'Vertex index: {vert_index}, UV coordinates: {uv}')
+        
+        bm.free()
+        data = pad_chunk(data)
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+        return data
+
+
+    def write_offset_table(kfcount, length):
+        data = bytearray()
+        data = set_dword(data, 0)
+
+        #нужно определить длину таблицы
+        table_length = (kfcount + 4) * 4
+        if table_length % 16 != 0:
+            table_length = ((table_length // 16) + 1) * 16
+
+        offset = 64 + table_length
+        data = set_dword(data, offset)
+
+        offset += len(texturedata)
+        data = set_dword(data, offset)
+
+        offset += len(facesdata)
+        data = set_dword(data, offset)
+
+        offset += len(uvdata)
+        data = set_dword(data, offset)
+
+        for kf in range(kfcount):
+            offset += length
+            data = set_dword(data, offset)
+
+        data = pad_chunk(data)
+        return data
+
+
+    #starting export
+    obj = bpy.context.active_object  #get active object
+
+    if obj and obj.type == 'MESH':
+        bpy.context.view_layer.objects.active = obj
+        bpy.ops.object.mode_set(mode='EDIT')
+
+        #Triangulating an object in a scene is a bad approach
+        #Rework needed!
+
+        bpy.ops.mesh.select_all(action='SELECT')
+        bpy.ops.mesh.quads_convert_to_tris(quad_method='BEAUTY', ngon_method='BEAUTY')
+
+        nvertices = len(obj.data.vertices)
+        texturedata = write_texture(texture_path)
+        facesdata, nfaces = get_triangle_representation(obj)
+        uvdata = get_uv_coords(obj)
+        animdata, kfcount, duration, length = get_keyframes(obj, kf_range[0], kf_range[1])
+        offsettable = write_offset_table(kfcount, length)
+        data_list = [offsettable, texturedata, facesdata, uvdata, animdata]
+        
+        mrfdata = bytearray()  
+        mrfdata = write_data_chunk(mrfdata, kfcount, nvertices, nfaces, duration)
+        
+        for data in data_list:
+            mrfdata.extend(data)
+        
+        with open(filepath, 'wb') as f:             
+            f.write(mrfdata) 
+    else:
+        MessageBox.show('Error!', 'No Active Object or Object type is not a Mesh', 'ERROR')
+        return
