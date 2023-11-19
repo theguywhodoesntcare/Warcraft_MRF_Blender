539500eac5ec5083b61d8c4da65c80bf4388774f
theguywhodoesntcare <stormshadow4101@gmail.com>
2023-11-19 18:04:42 +0300

    mdx mesh to mrf mesh C# converter

---
+++ mdx2mrf/Program.cs
@@ -0,0 +1,41 @@
+﻿using mdx2mrd;
+using MdxLib.Model;
+using MdxLib.ModelFormats;
+using System;
+using System.IO;
+
+namespace mdx2mrf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //args = new string[] { @"C:\Users\Administrator\Desktop\KingThrone.mdx" };
+
+            if (args.Length == 0)
+                return;
+
+            string filePath = args[0];
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Файл не существует.");
+                return;
+            }
+
+            CModel Model = new CModel();
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                new CMdx().Load(filePath, (Stream)fileStream, Model);
+
+                byte[] mrfData = Parser.Parse(Model);
+
+                string savePath = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + "_new.mrf");
+
+                if (mrfData != null) File.WriteAllBytes(savePath, mrfData);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
